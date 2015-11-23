4172591c847f6040c0b9b91688e75f338ef68391
trindax19 <ricardo.trindade743@gmail.com>
2015-11-23 22:37:06 +0000

    Changes in h19

--- changepan.php
+++ changepan.php
@@ -8,11 +8,26 @@
 	$dbname = $user;	// Do nothing here, your database has the same name as your username.
 	$time_start = $_REQUEST['timestamp_start'];
 	$time_end =$_REQUEST['timestamp_end'];
-
+	$pan=$_SESSION['domain'];
 	
 	
 
  $connection = new PDO("mysql:host=" . $host. ";dbname=" . $dbname, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+
+
+ 	$sqltest="SELECT start, end from Wears
+	where pan='$pan'
+	and end >=all(select end from Wears
+	where pan='pan003.healthunit.org');";
+
+	$checktime =$connection->query($sqltest);
+	foreach ($checktime as $row) {
+		$t1=$row['start'];
+		$t2=$row['end'];
+	}
+	if($t2<$time_end || $t1>$time_start){
+			echo "Invalid Period";
+	}else{
  $sqlperiod ="INSERT into Period values ('$time_start','$time_end');"; 
  $addtime = $connection->exec($sqlperiod);
 	foreach($_REQUEST as $name => $value)
@@ -36,5 +51,6 @@ echo("<p>Devices have been transfered!!!");
 
 }
 	$connection=null;
+}
 	?>
 </html>
